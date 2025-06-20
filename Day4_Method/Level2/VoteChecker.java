9bbfa624805bfe0f8e2949411ee99ef3f285ac0e
Agastya kaushik <kaushik.agastya72@gmail.com>
2025-06-20 11:03:38 +0530

    level 2

--- Day4_Method/Level2/VoteChecker.java
+++ Day4_Method/Level2/VoteChecker.java
@@ -19,7 +19,7 @@ public class VoteChecker {
      public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         int []totalpeople=new int[10];
-        System.err.println("enter people ag");
+        System.err.println("enter people age");
         VoteChecker obj=new VoteChecker();
         for(int i=0;i<=totalpeople.length-1;i++){
             totalpeople[i]=scanner.nextInt();
