3b926d982bf62bfef8a382e9a455b600409f1576
Agastya kaushik <kaushik.agastya72@gmail.com>
2025-08-28 10:47:42 +0530

    linear and binary search

---
+++ Day4Searching/Linear/FirstNegativeLinearSearch.java
@@ -0,0 +1,8 @@
+public class FirstNegativeLinearSearch {
+    public int findFirstNegative(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0) return i;
+        }
+        return -1;
+    }
+}
