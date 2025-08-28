d3194a858402ceca78644764a77e03c8c66d01d9
Agastya kaushik <kaushik.agastya72@gmail.com>
2025-08-28 15:24:34 +0530

    pseudo code

--- Day4Searching/Linear/FirstNegativeLinearSearch.java
+++
@@ -1,8 +0,0 @@
-public class FirstNegativeLinearSearch {
-    public int findFirstNegative(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < 0) return i;
-        }
-        return -1;
-    }
-}
--- Day4Searching/StringBuilder/ReverseStringBuilder.java
+++
@@ -1,6 +0,0 @@
-public class ReverseStringBuilder {
-    public String reverse(String input) {
-        StringBuilder sb = new StringBuilder(input);
-        return sb.reverse().toString();
-    }
-}
